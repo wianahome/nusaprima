120a44983c0ab36f6f20d8d8c6c47cceadf856ab
Wiana <wianahome@gmail.com>
2026-05-02 14:36:44 +0800

    Google ads tracking code

--- lib/google-ads.ts
+++ lib/google-ads.ts
@@ -2,7 +2,7 @@
 export const reportWaConversion = () => {
   if (typeof window !== 'undefined' && (window as any).gtag) {
     (window as any).gtag('event', 'conversion', {
-      'send_to': 'AW-17941029885/kISECL_ivaAcEP3H-epC', // Gunakan ID & Label yang sama untuk semua tombol WA
+      'send_to': 'AW-18134268066/d-8ZCI_liKYcEKLxi8dD', // Gunakan ID & Label yang sama untuk semua tombol WA
     });
   }
 };
